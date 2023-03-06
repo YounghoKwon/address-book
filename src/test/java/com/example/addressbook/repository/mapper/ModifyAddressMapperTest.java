f5e60b5f4918735563a07c874855f0a1219639ff
권영호 <dudgh0958@pood.pet>
2023-03-06 14:20:29 +0900

    테스트 코드 작성

---
+++ src/test/java/com/example/addressbook/repository/mapper/ModifyAddressMapperTest.java
@@ -0,0 +1,16 @@
+package com.example.addressbook.repository.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ModifyAddressMapperTest {
+    @Test
+    @DisplayName("ModifyAddressMapper 생성 테스트")
+    void of(){
+        ModifyAddressMapper modifyAddressMapper = ModifyAddressMapper.of(1, "홍길동", 01011114444);
+        Assertions.assertThat(modifyAddressMapper.getIdx()).isEqualTo(1);
+    }
+}
